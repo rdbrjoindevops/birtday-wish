60634129213b5d7304b6b331e5b59568e0c19ef3
ravindra <rdbrjoindevops@gmail.com>
2024-12-30 20:56:40 +0530

    happy birthday

---
+++ kim_birthday.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+
+echo ""Happy Birthday to my sweet kim. Watching you grow has been an absolute joy. May your day be filled with magic and wonder."
+"To my amazing niece, on your special day: May your life be filled with happiness, success, and love. I'm so proud of you!"
+"Happy Birthday, Mihira! You're such a bright light in our lives. Wishing you a lifetime of happiness and cherished memories.""
